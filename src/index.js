956eb183813dd780403a9b8b55dc68de71252a11
Souvik Karmakar <rajkarmakar892@gmail.com>
2023-08-06 19:41:40 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles.css"; // Add this line to import custom CSS
+import "./styles.css";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
